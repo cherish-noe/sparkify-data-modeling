7074c77665ab22d05309dca11e8ec392d018ce50
Myat77 <myatnoe.luck@gmail.com>
2021-07-07 23:41:57 +0630

    Update code

--- etl.py
+++ etl.py
@@ -8,126 +8,129 @@ from sql_queries import *
 def process_song_file(cur, filepath):
     """
     Process the song file to insert data into the specific data model for analysis
-    @param cur: the database cursor
-    @param filepath: the path to the song file
+    :param cur: psycopg2 database connection cursor
+    :param filepath: file path of song data
     """
-
-    # open song file
-    df = pd.read_json(filepath, type='series')
-
-    # insert song record
-    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']]
-    song_data = list(song_data.values)
-    cur.execute(song_table_insert, song_data)
-
-    # insert artist record
-    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']]
-    artist_data = list(artist_data.values)
-    cur.execute(artist_table_insert, artist_data)
-
+    
+    # Open song file
+    df = pd.read_json(filepath, lines=True)
+      
+    # Insert artist record
+    artist_data_df = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']]
+    artist_data_values = artist_data_df.values
+    first_record_df = artist_data_values[0]
+    artist_data = first_record_df.tolist()
+    
+    try:
+        cur.execute(artist_table_insert, artist_data)
+    except psycopg2.Error as error:
+        print(error)   
+        
+    # Insert song record
+    song_data_df = df[['song_id', 'title', 'artist_id', 'year', 'duration']]
+    song_data_values = song_data_df.values
+    first_record_df = song_data_values[0]
+    song_data = first_record_df.tolist()
+    
+    try:
+        cur.execute(song_table_insert, song_data)
+    except psycopg2.Error as error:
+        print(error)
+        
 
 def process_log_file(cur, filepath):
     """
-    Process the log file to insert data into the specific data model for data analysis
-    @param cur: psycopg2 connection cursor
-    @param filepath: the path to the log file
-    """ 
-
-    # open log file 
+    Process the log file to insert data into the specific data model for analysis
+    :param cur: psycopg2 connection cursor
+    :param filepath: file path of data to wrangle
+    """
+    
+    # Open log file
     df = pd.read_json(filepath, lines=True)
 
-    # filter by NextSong action
-    df = df[df.page == 'NestSong']
+    # Filter by NextSong action
+    df = df[df.page == 'NextSong']
 
-    # convert timestamp column to datetime
+    # Convert timestamp column to datetime
     t = pd.to_datetime(df['ts'], unit='ms')
+    
+    # Insert time data records
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ['timestamp', 'hour', 'day', 'weekofyear', 'month', 'year', 'weekday']
+    time_dict = dict(zip(column_labels, time_data))
+    time_df = pd.DataFrame(time_dict) 
 
-    # insert time record
-    time_values = [
-        t,
-        t.dt.hour,
-        t.dt.day,
-        t.dt.weekofyear,
-        t.dt.month,
-        t.dt.year,
-        t.dt.weekday
-    ]
-    column_labels = [
-        'timestamp',
-        'hour',
-        'day',
-        'weekofyear',
-        'month',
-        'year',
-        'weekday'
-    ]
-    time_dict = dict(zip(column_labels, time_values))
-    time_data = pd.DataFrame(time_dict)
+    for i, row in time_df.iterrows():
+        try:
+            cur.execute(time_table_insert, list(row))
+        except psycopg2.Error as error:
+            print(error)
 
-    for i, row in time_data.iterrows():
-        cur.execute(time_table_insert, list(row))
+    # Load user table
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+    user_df = user_df.drop_duplicates().dropna()
 
-    # load user table
-    user_data = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
-    user_data = user_data.drop_duplicates().dropna()
+    # Insert user records
+    for i, row in user_df.iterrows():
+        try:
+            cur.execute(user_table_insert, row)
+        except psycopg2.Error as error:
+            print(error)    
 
-    # insert user record
-    for i, row in user_data.iterrows():
-        cur.execute(user_table_insert, row)
-
-    # insert songplay record
+    # Insert songplay records
     for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
+
+        # Get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
-        results = cur.fechone()
+        results = cur.fetchone()
 
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
-        # insert songplay record
+        # Insert songplay record
         songplay_data = (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
-        cur.execute(song_table_insert, songplay_data)
+        cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     """
-    Read all the files in 'filepath' and call the 'func' function
-    @param cur: the database server
-    @param conn: the database connection
-    @param filepath: the path to the data directory
-    @param func: the function (process songs or logs)
+    Read all the files in `filepath` and call the `func` function
+    :param cur: Coneected database cursor
+    :param conn: psycopg2 connection object
+    :param filepath: file path of the data to be processed
+    :param func: function to trigger on the data
     """
-
-    # get all files matching extensions from directory
+    
+    # Get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, '*.json'))
-        for f in files:
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
             all_files.append(os.path.abspath(f))
-    
-    # get total number of files found
+
+    # Get total number of files found
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
 
-    # iterate over files and process
+    # Iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
 
 
-def insert_songs_and_logs():
+def main():
     """
     Insert songs and logs to our custom database.
     """
+    
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=myatnoe")
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='./data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='./data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath='../data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='../data/log_data', func=process_log_file)
 
     conn.close()
 
